acea7131095045c83bb27bf66185ac51a69dca8e
Brian A. Wandell <wandelL@stanford.edu>
2013-08-09 23:19:12 -0700

    New functions from iset for sceneFromFile and for illustrating new OLED devices.
    Signed-off-by: Brian A. Wandell <wandelL@stanford.edu>

---
+++ isettools/utility/image/ieLUTInvert.m
@@ -0,0 +1,36 @@
+function lut = ieLUTInvert(inLUT,resolution)
+% Invert a lookup table (lut) at a specified sampling resolution
+%
+%    lut = ieLUTInvert(inLUT,resolution)
+%
+% inLUT:      A gamma table that converts linear DAC values to linear RGB.
+% resolution: The display bit depth is log2(size(DAC,1)).  We are going to
+%   make an inverse table with finer resolution.  
+%
+% lut:  The returned lookup table.
+% If resolution = 2, then we have twice the number of levels in the
+% returned table.
+%
+% Example:
+%   d = displayCreate;
+%   inLUT = d.gamma.^0.6;
+%   lut = ieLUTInvert(inLUT,3);
+%   vcNewGraphWin; plot(lut)
+%
+% See also:  ieLUTDigital, ieLUTLinear
+%
+% (c) Imageval Consulting, LLC 2013
+
+if ieNotDefined('inLUT'), error('input lut required'); end
+if ieNotDefined('resolution'), resolution = 0.5; end
+
+x = 1:size(inLUT,1);
+y = inLUT(:,1);
+
+nbits = log2(length(y));
+m = 2^nbits - 1;
+
+iY = (0:(1/resolution):m)/(2^nbits);
+lut = interp1(y,x,iY,'cubic',m);
+
+end
