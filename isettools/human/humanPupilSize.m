b724be02cadf3f177c380928a81b82a5a5df2183
Haomiao Jiang <hjiang36@gmail.com>
2014-05-27 21:34:09 -0700

    Update humanPupilSize function
    Add methods:
    ‘sd’  - Stanley and Davies (1995)
    ‘wy’ - Watson and Yellott(2012)

--- isettools/human/humanPupilSize.m
+++ isettools/human/humanPupilSize.m
@@ -1,14 +1,26 @@
-function [diam,area] = humanPupilSize(lum,model)
+function [diam, area] = humanPupilSize(lum, model, area, age, eyeNum)
 % Estimated pupil diameter and area (mm^2) 
 %
-%  [diam,area] = pupilSize(lum,'model')
+%  [diam,area] = pupilSize(lum,'model', [area], [eyeNum])
+%
+% Inputs:
+%   lum    - luminance in cd/m^2 or cd/m^2/deg^2 for 'sd' and 'wy' model
+%   model  - string of selected model, see notes below for detail
+%   area   - optional parameter, used in 'sd' and 'wy' model. Should be in
+%            units of deg^2
+%   age    - age of the subject in years
+%   eyeNum - binocular (2) or monocular (1), used in 'wy' model
 %
 % The mean luminance is in candelas/m2.  The returned diameter and pupil
 % area are in mm and mm^2, respectively.
 %
-% Two models implemented.
+% Different models implemented:
 %    'ms'  - Moon and Spencer (1944)
 %    'dg'  - DeGroot and Gebhard (1952)
+%    'sd'  - Stanley and Davies (1995)
+%    'wy'  - Watson and Yellott (2012)
+%
+% For some models, it requires more parameters than just luminance and 
 %
 % Related note: 
 %   According to James E. Birren, Roland C. Casperon, & Jack Botwinick,
@@ -27,7 +39,18 @@ function [diam,area] = humanPupilSize(lum,model)
 %      D = 7.75-5.75 [(F/846)0.41/((F/846)0.41 / 2)] 
 %   
 %   where D is the pupil diameter (mm) and F is the corneal flux density
-%   (cdm-2 deg2). Not implemented here, but perhaps it should be.
+%   (cdm-2 deg2).
+%
+%   According to Andrew B. Watson and John I. Yellott (A unified formula
+%   for light-adapted pupil size) The pupil size formula should incorporate
+%   the combined effects of the observer's age, the size of adapting field,
+%   and number of eyes used (mono- / bi-nocular). They describe the formula
+%   as:
+%     D = D_sd(F,1) + (y-y0)*(0.02132 - 0.009562 * D_sd(F,1));
+%
+%   Where D is the pupil size, y is the age, y0 is the reference age
+%         F = LaM(e), a is the field area in deg^2, L is the luminance in
+%         cd/m^2
 % 
 % Example:
 %    [d,a] = humanPupilSize(100,'ms')
@@ -50,7 +73,21 @@ switch lower(model)
         diam = 4.9 - 3*tanh(0.4*log10(lum) + 1);
     case 'dg'
         diam = 10.^(0.8558 - 0.000401*(log10(lum) + 8.6).^3);
-    case 'agemin'
+    case 'sd'
+        if notDefined('area'), error('Area in deg^2 required'); end
+        F = lum * area;
+        diam = 7.75 - 5.75 * (F/846)^0.41 / ((F/846)^0.41 + 2);
+    case 'wy'
+        if notDefined('area'), error('Area in deg^2 required'); end
+        if notDefined('age'), age = 28; end
+        if eyeNum == 1
+            me = 0.1;
+        elseif eyeNum == 2
+            me = 1;
+        end
+        F = lum * area * me;
+        Dsd = humanPupilSize(F, 'sd', 1);
+        diam = Dsd + (age - 28.58)*(0.02132 - 0.009562*Dsd);
     otherwise
         error('Unknown model')
 end
