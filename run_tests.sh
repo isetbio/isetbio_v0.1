821ec5e29ef369b1ba4297135ffb4905ef28eae6
Haomiao Jiang <hjiang36@gmail.com>
2014-07-21 11:35:34 -0700

    update to run_test.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,10 @@
-exit 0
+# Run unit test in matlab
+/Applications/MATLAB_R2014a.app/bin/matlab -nodesktop -nosplash -nodisplay -r "unitTest"
+if [ "$?" == "0" ];
+then
+    echo Unit test passed!
+    exit 0
+else
+    echo Unit Test Failed
+    exit 1
+fi
---
+++ unitTest.m
@@ -0,0 +1,15 @@
+function unitTest
+%% Run unit test
+%    result = unitTest
+%
+%
+%  (HJ) ISETBIO TEAM, 2014
+
+try
+    addpath(genpath(isetbioRootPath));
+    exit(0);
+catch
+    exit(-1);
+end
+
+end
