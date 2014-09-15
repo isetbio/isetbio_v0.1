8b60cb6dd2f1d542d7ddc011e92f90400b0c5dad
Nicolas Cottaris <cottaris@sas.upenn.edu>
2014-09-15 15:51:33 -0400

    Various improvements in @UnitTest.

--- validation/@UnitTest/addProbe.m
+++ validation/@UnitTest/addProbe.m
@@ -5,12 +5,14 @@ function addProbe(obj, varargin)
     p.addParamValue('functionHandle', []);
     p.addParamValue('functionParams', @isstruct);
     p.addParamValue('generatePlots',  @islogical);
+    p.addParamValue('onErrorReactBy', @ischar); 
     p.parse(varargin{:});
     
     % get input params
     newProbe.name = p.Results.name;
     newProbe.functionHandle = p.Results.functionHandle;
     newProbe.functionParams = p.Results.functionParams;
+    newProbe.onErrorReactBy = p.Results.onErrorReactBy;
     
     if (isa(newProbe.functionHandle, 'function_handle') == false)
        error('The value of key:''functionHandle'' is not a valid function handle');
@@ -18,7 +20,13 @@ function addProbe(obj, varargin)
     
     % attempt to run the probe
     probeCommandString = sprintf('validationData = newProbe.functionHandle(newProbe.functionParams);');
-    command = sprintf('try \n\t%s \n\t newProbe.result.validationData = validationData; newProbe.result.status = ''OK''; \ncatch err \n\t newProbe.result.status = ''failed''; newProbe.result.message = err.message; \nend', probeCommandString);
+    if (strcmp(newProbe.onErrorReactBy, 'CatchingExcemption'))
+        command = sprintf('try \n\t%s \n\t newProbe.result.validationData = validationData; newProbe.result.status = ''OK''; \ncatch err \n\t newProbe.result.status = ''failed''; newProbe.result.message = err.message; \nend', probeCommandString);
+    elseif (strcmp(newProbe.onErrorReactBy, 'RethrowingExcemption'))
+        command = sprintf('try \n\t%s \n\t newProbe.result.validationData = validationData; newProbe.result.status = ''OK''; \ncatch err \n\t newProbe.result.status = ''failed''; newProbe.result.message = err.message; \n rethrow(err); \nend', probeCommandString);
+    else
+        error('''onErrorRectBy'':%s is an invalid mode. Choose either ''CatchingExcemption'' or ''RethrowingExcemption''.', newProbe.onErrorReactBy);
+    end
     eval(command);
     
     % add the probe result
@@ -33,7 +41,7 @@ function addProbe(obj, varargin)
     else
         fprintf('\n%2d. \t Name\t\t\t: ''%s'' \n', pIndex, obj.probesPerformed{pIndex}.name);
         fprintf('\t ValidationScript\t:  %s.m\n', func2str(newProbe.functionHandle));
-        fprintf('\t Status\t\t\t:  Success. No excemption was raised during execution.\n');
+        fprintf('\t Status\t\t\t:  Success with report: %s.\n', newProbe.result.validationData.report);
     end
     
 end
