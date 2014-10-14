418d96352ad6252e3ce53911971a487353ac478c
Haomiao Jiang <hjiang36@gmail.com>
2014-10-13 17:19:24 -0700

    update to v_rieke

--- cones/s_fredConeModel.m
+++ cones/s_fredConeModel.m
@@ -46,9 +46,6 @@ Stm(1600) = FlashAmp;
 Stm(2200) = FlashAmp;
 Stm(2600) = FlashAmp;
 
-% Why is this created and then cleared in the next step?
-
-
 %%  Create a stimulus ... think more about the choice here.
 % inc/dec asymmetry
 clear Stm;
@@ -61,8 +58,8 @@ Stm((s(1)+1):s(2)) = MeanAmp - StepAmp;
 Stm((s(2)+1):s(3)) = MeanAmp;
 Stm((s(3)+1):s(4)) = MeanAmp + StepAmp;
 
-vcNewGraphWin;
-plot(Stm); grid on;
+% vcNewGraphWin;
+% plot(Stm); grid on;
 
 % Save this out for comparison with ISETBIO
 riekeStim1 = Stm;
@@ -80,43 +77,43 @@ cslow(1) = cdark;
 for pnt = 2:NumPts
     r(pnt) = r(pnt-1) + TimeStep * (-sigma * r(pnt-1) + Stm(pnt));
 	p(pnt) = p(pnt-1) + TimeStep * (r(pnt-1) + eta - phi * p(pnt-1));
-	c(pnt) = c(pnt-1) + TimeStep * (cur2ca * 2* cgmp2cur * g(pnt-1)^3/(1+cslow(pnt-1)/cdark) - beta * c(pnt-1));
+	c(pnt) = c(pnt-1) + TimeStep * (cur2ca * cgmp2cur * g(pnt-1)^3 - beta * c(pnt-1));
 	cslow(pnt) = cslow(pnt-1) - TimeStep * (betaSlow * (cslow(pnt-1)-c(pnt-1)));
 	s(pnt) = smax / (1 + (c(pnt) / hillaffinity)^hillcoef);
 	g(pnt) = g(pnt-1) + TimeStep * (s(pnt-1) - p(pnt-1) * g(pnt-1));
 end
 % determine current change
-cur = -2 * cgmp2cur * g.^3 * 1 ./ (1 + cslow ./ cdark);
+cur = -cgmp2cur * g.^3 * 2 ./ (2 + cslow ./ cdark);
 riekeCurrent1 = cur;
 
-% plot current, pde, synthesis, cGMP and calcium
-figure(1); clf;
-subplot(6, 1, 1);
-plot(tme, cur);
-xlabel('time (sec)');
-ylabel('current');
-subplot(6, 1, 2);
-plot(tme, p);
-xlabel('time (sec)');
-ylabel('pde activity');
-subplot(6, 1, 3);
-plot(tme, s);
-xlabel('time (sec)');
-ylabel('synthesis rate');
-subplot(6, 1, 4);
-plot(tme, g);
-xlabel('time (sec)');
-ylabel('[cGMP]');
-subplot(6, 1, 5) 
-plot(tme, c)
-xlabel('time (sec)');
-ylabel('[calcium]');
-subplot(6, 1, 6) 
-plot(tme, cslow)
-xlabel('time (sec)');
-ylabel('[calcium slow]');
-
-figure(2);clf
+% % plot current, pde, synthesis, cGMP and calcium
+% figure(1); clf;
+% subplot(6, 1, 1);
+% plot(tme, cur);
+% xlabel('time (sec)');
+% ylabel('current');
+% subplot(6, 1, 2);
+% plot(tme, p);
+% xlabel('time (sec)');
+% ylabel('pde activity');
+% subplot(6, 1, 3);
+% plot(tme, s);
+% xlabel('time (sec)');
+% ylabel('synthesis rate');
+% subplot(6, 1, 4);
+% plot(tme, g);
+% xlabel('time (sec)');
+% ylabel('[cGMP]');
+% subplot(6, 1, 5) 
+% plot(tme, c)
+% xlabel('time (sec)');
+% ylabel('[calcium]');
+% subplot(6, 1, 6) 
+% plot(tme, cslow)
+% xlabel('time (sec)');
+% ylabel('[calcium slow]');
+% 
+% figure(2);clf
 % plot(cur(5000:length(cur)) - cur(1000)); hold on
 % plot(Resp - Resp(1000), 'r');
 
@@ -125,7 +122,7 @@ figure(2);clf
 % plot(tme,BaselineSubtraction(Resp,8000,9000),'k')
 % hold off
 
-plot(tme,(cur))
+% plot(tme,(cur))
 % hold on
 % plot(normalize(Resp))
 % hold off
@@ -136,15 +133,15 @@ plot(tme,(cur))
 % plot(cur(3*NumPts/4:3*NumPts/4+800), 'r');
 
 
-figure(3)
-plot(tme,Stm)
+% figure(3)
+% plot(tme,Stm)
 %%
 % solve difference equations for step or pulse
 clear Stm;
 
 StmPts = 5;
 StmAmp = 1000;
-NumPts = 800;
+NumPts = 1000;
 tme = 1:NumPts;
 
 clear g s c p cslow;
@@ -178,29 +175,29 @@ riekeCurrent2 = cur;
 riekeStim2 = Stm;
 
 % plot current, pde, synthesis, cGMP and calcium
-figure(1); clf;
-subplot(5, 1, 1);
-plot(tme, p);
-xlabel('time (sec)');
-ylabel('pde activity');
-subplot(5, 1, 2);
-plot(tme, s);
-xlabel('time (sec)');
-ylabel('synthesis rate');
-subplot(5, 1, 3);
-plot(tme, g);
-xlabel('time (sec)');
-ylabel('[cGMP]');
-subplot(5, 1, 4) 
-plot(tme, c)
-xlabel('time (sec)');
-ylabel('[calcium]');
-subplot(5, 1, 5) 
-plot(tme, cslow)
-xlabel('time (sec)');
-ylabel('[calcium slow]');
-
-figure(2); clf;
-plot(cur);
+% figure(1); clf;
+% subplot(5, 1, 1);
+% plot(tme, p);
+% xlabel('time (sec)');
+% ylabel('pde activity');
+% subplot(5, 1, 2);
+% plot(tme, s);
+% xlabel('time (sec)');
+% ylabel('synthesis rate');
+% subplot(5, 1, 3);
+% plot(tme, g);
+% xlabel('time (sec)');
+% ylabel('[cGMP]');
+% subplot(5, 1, 4) 
+% plot(tme, c)
+% xlabel('time (sec)');
+% ylabel('[calcium]');
+% subplot(5, 1, 5) 
+% plot(tme, cslow)
+% xlabel('time (sec)');
+% ylabel('[calcium slow]');
+% 
+% figure(2); clf;
+% plot(cur);
 
 
--- scripts/v_rieke.m
+++ scripts/v_rieke.m
@@ -6,12 +6,54 @@
 % HJ/BW ISETBIO Team, Copyright 2014
 
 
-%% The Rieke code is
-
+%% The Rieke code
 % This produces riekeStim1, riekeStim2, riekeCur1, riekeCur2
-s_fredConeModel
-plot(riekeStim1)
-plot(riekeStim2)
-plot(riekeCurrent1)
-plot(riekeCurrent2)
+s_fredConeModel;
 
+%% ISETBIO Implementation
+%  create human sensor structure
+sensor = sensorCreate('human');
+
+% set photon absorptions
+sensor = sensorSet(sensor, 'size', [1 1]); % Just 1 cone
+nSample = length(riekeStim1);
+photons = riekeStim1 * sensorGet(sensor, 'exp time');
+volts = photons * sensorGet(sensor, 'conversion gain');
+sensor = sensorSet(sensor, 'volts', reshape(volts, [1 1 nSample]));
+
+% compute adaptation
+params.bgVolts = 0;
+[~, isetbioCur1] = coneAdapt(sensor, 4, params);
+
+% set photons of stimulus 2 (Impulse)
+photons = riekeStim2 * sensorGet(sensor, 'exp time');
+volts = photons * sensorGet(sensor, 'conversion gain');
+sensor = sensorSet(sensor, 'volts', reshape(volts, [1 1 nSample]));
+
+% compute adaptation
+[~, isetbioCur2] = coneAdapt(sensor, 4, params);
+
+%% Plot and compare the results
+% new figure
+vcNewGraphWin;
+
+% plot stimulus
+subplot(2,2,1); hold on; grid on;
+plot(1:nSample, riekeStim1, 'r', 'lineWidth', 2);
+xlabel('time (ms)'); ylabel('Photons per second');
+subplot(2,2,2); hold on; grid on;
+plot(1:nSample, riekeStim2, 'r', 'lineWidth', 2);
+xlabel('time (ms)'); ylabel('Photons per second');
+
+% plot adapted current
+subplot(2,2,3); hold on; grid on;
+plot(1:nSample, riekeCurrent1, 'r', 'lineWidth', 2);
+plot(1:nSample, isetbioCur1(:), '--b', 'lineWidth', 2);
+xlabel('time (ms)'); ylabel('Adapted Current');
+legend('Rieke', 'ISETBIO');
+
+subplot(2,2,4); hold on; grid on;
+plot(1:nSample, riekeCurrent2, 'r', 'lineWidth', 2);
+plot(1:nSample, isetbioCur2(:), '--b', 'lineWidth', 2);
+xlabel('time (ms)'); ylabel('Adapted Current');
+legend('Rieke', 'ISETBIO');
