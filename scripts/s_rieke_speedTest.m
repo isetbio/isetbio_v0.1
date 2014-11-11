aecffa50a1679ee278757b0e5bc5baff51ea8f02
Haomiao Jiang <hjiang36@gmail.com>
2014-11-10 23:22:53 -0800

    Update speed test script

--- scripts/s_rieke_speedTest.m
+++ scripts/s_rieke_speedTest.m
@@ -14,14 +14,14 @@ expTime = 0.05;  % integration time 50 ms
 sensor = sensorCreate('human');
 sensor = sensorSet(sensor, 'exposure time', expTime);
 sensor = sensorSet(sensor, 'time interval', deltaT);
-oi = oiCreate('human');
+sensor = sensorSet(sensor, 'size', [100 100]); % make it a square
 
 %% Small test case
 %  This should finish within a couple of seconds
 duration = 1; % 1 second
 fov = 1; % 1 deg
 
-sensor = sensorSetSizeToFOV(sensor, fov, [], oi);
+sensor = sensorSetSizeToFOV(sensor, fov);
 sz = sensorGet(sensor, 'size');
 
 % create random input
@@ -33,25 +33,25 @@ sensor = sensorSet(sensor, 'photon rate', pRate);
 % this computation uses a default background (initial) isomerizaiton rate
 % equals to the mean of the stimulus
 tic;
-[~, cur_small] = coneAdapt(sensor, 4);
+[~, cur_small] = coneAdapt(sensor, 'rieke');
 toc;
 
 %% Large test cases
 %  This could takes you 10 minutes if there is no 'not enough memory' error
-duration = 1; % 1 second
-fov = 10; % 10 x 10 deg, more than 3M cones
-
-sensor = sensorSetSizeToFOV(sensor, fov, [], oi);
-sz = sensorGet(sensor, 'size');
-
-% create random input
-% the random isomerization rate is controlled within 1000 to 100000
-pRate = 99000 * rand([sz duration/deltaT]) + 1000;
-sensor = sensorSet(sensor, 'photon rate', pRate);
-
-% compute adapted current
-% this computation uses a default background (initial) isomerizaiton rate
-% equals to the median of the stimulus
-tic;
-[~, cur_large] = coneAdapt(sensor, 4);
-toc;
+% duration = 1; % 1 second
+% fov = 10; % 10 x 10 deg, more than 3M cones
+% 
+% sensor = sensorSetSizeToFOV(sensor, fov);
+% sz = sensorGet(sensor, 'size');
+% 
+% % create random input
+% % the random isomerization rate is controlled within 1000 to 100000
+% pRate = 99000 * rand([sz duration/deltaT]) + 1000;
+% sensor = sensorSet(sensor, 'photon rate', pRate);
+% 
+% % compute adapted current
+% % this computation uses a default background (initial) isomerizaiton rate
+% % equals to the median of the stimulus
+% tic;
+% [~, cur_large] = coneAdapt(sensor, 'rieke');
+% toc;
