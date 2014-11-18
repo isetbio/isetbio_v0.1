5a8a6c4ec54f63b7776f1303aafaea361f22873f
np_cottaris <cottaris@sas.upenn.edu>
2014-11-18 17:23:40 -0500

    Minor fix.

--- validation/InDevelopment/initializeRunParams.m
+++ validation/InDevelopment/initializeRunParams.m
@@ -6,12 +6,17 @@ function runParams = initializeRunParams(varargin)
     else
         assert(nargin == 1);
         runParams = varargin{1};
-        assert(isstruct(runParams));
-        if (~isfield(runParams, 'generatePlots'))
-            runParams.generatePlots = true;
+        if (isstruct(runParams))
+            if (~isfield(runParams, 'generatePlots'))
+                runParams.generatePlots = true;
+            end
+            if (~isfield(runParams, 'printValidationReport'))
+                runParams.printValidationReport = true;
+            end
         end
-        if (~isfield(runParams, 'printValidationReport'))
-            runParams.printValidationReport = true;
+        if (isempty(runParams))
+           runParams.generatePlots = true; 
+           runParams.printValidationReport = true;
         end
     end
 end
