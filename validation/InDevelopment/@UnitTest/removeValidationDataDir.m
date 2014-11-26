6934f76de8fe19f28511bdddd9b0bbfcee8588c9
np_cottaris <cottaris@sas.upenn.edu>
2014-11-26 01:43:50 -0500

    Moved UnitTest

--- validation/InDevelopment/@UnitTest/removeValidationDataDir.m
+++
@@ -1,8 +0,0 @@
-% Method to remove the root validationData directory
-function removeValidationDataDir(obj)
-    if (exist(obj.validationDataDir, 'dir'))
-        rmpath(obj.validationDataDir);
-    end
-    system(sprintf('rm -r -f %s', obj.validationDataDir));
-end
-        
--- validation/InDevelopment/@UnitTest/structsAreSimilar.m
+++
@@ -1,4 +0,0 @@
-function result = structsAreSimilar(obj, groundTruthData, validationData)
-    % disp('Need to implement ''StructsAreSimilar(groundTruthData, validationData)'' ');
-    result = true;
-end
