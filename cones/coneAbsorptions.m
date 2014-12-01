79dff1c540110075adc847748cff93587b869ea7
Brian Wandell <wandell@stanford.edu>
2014-11-30 16:32:24 -0800

    Comments updated

--- cones/coneAbsorptions.m
+++ cones/coneAbsorptions.m
@@ -1,32 +1,36 @@
 function sensor = coneAbsorptions(sensor, oi, varargin)
-% Compute the cone absorptions accounting for eye movements
+% Compute a temporal array of cone absorptions including eye movements
 %
-%   sensor = coneAbsorptions(sensor, oi, [showBar]);
+%   sensor = coneAbsorptions(sensor, oi);
 %
-% Loop through a number of eye position fixations and calculate the voltage
-% image at each position, concatenating the voltage images to
-% (row,col,total nSamp).
+% This function is an extended form of sensorCompute, which was designed
+% for a single capture.  This one loops through a series of eye fixations
+% and calculates the voltage image at each position, concatenating the
+% voltage images to (row,col,total nSamp).  The calculation for each
+% position uses sensorCompute.
 %
-% The computational strategy is for each fixation, first calculate
-% the mean (noiseless) voltage and then add frame-by-frame noise. To move
-% the sensor, we expand it using sensorHumanResize, and then remove
-% unneeded rows/columns from the outputted volatge image.
+% The computational strategy is for each fixation, first calculate the mean
+% (noiseless) voltage and then add frame-by-frame noise. The reason for
+% this strategy is that sometimes the eye positions are the same.  By only
+% using sensorCompute() for the different positions, we save computational
+% cost.
 %
-% The last parameter showBar indicates whether or not to show the progress
-% bar. 0 or false for hiding all. true is for displaying the waitBar. Any
-% value larger than 2 is for printing progress information into command
-% window
+% To simulate eye (sensor) movements, we expand the sensor using
+% sensorHumanResize to be large enough to account for all possible eye
+% positions. We compute with the large image and then remove unneeded
+% rows/columns from the output voltage image.
 %
-% Example: A horizontal eye movement, 2% of the scene FOV
+% Example: An eye movement example
+%
 %   scene = sceneCreate;
 %   oi = oiCreate;
 %   oi = oiCompute(scene,oi);
 %   sensor = sensorCreate('human');
-%   xy = [0 0.02; 0 0]* sensorGet(sensor, 'fov'); %(x,y) positions
-%   framesPerPosition = [25 25];  % N frames at each (x,y) position
-%   sensor = sensorSet(sensor,'frames per position',framesPerPosition);
-%   sensor = sensorSet(sensor,'movement positions',xy);
+%   positions = randi(5,20,2);    % Units are number of cones
+%   sensor = sensorSet(sensor,'sensor positions',positions);
 %   sensor = coneAbsorptions(sensor, oi);
+%   v = sensorGet(sensor,'volts');
+%   vcNewGraphWin; implay(v/max(v(:)))
 %
 % See also:  s_rgcScene2Cones, sensorHumanResize, sensorComputeSamples
 %
