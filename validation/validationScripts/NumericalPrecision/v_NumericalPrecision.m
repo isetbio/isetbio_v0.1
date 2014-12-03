bb78f1c3150aa64eaaa3aceeed7148d3089499aa
Nicolas Cottaris <cottaris@sas.upenn.edu>
2014-12-03 14:54:56 -0500

    Work on numerical precision.

--- validation/validationScripts/NumericalPrecision/v_NumericalPrecision.m
+++ validation/validationScripts/NumericalPrecision/v_NumericalPrecision.m
@@ -36,51 +36,81 @@ function ValidationScript(runTimeParams)
     
     %% Internal validations
     % The result of a computation: a vector of doubles
-    result = 0 + eps*rand(1,10000);
-
-    decimalDigits = [3:20]+5;
+    gain = 10E18;
+    largeResult = gain*eps*rand(1,100*1000);
+    largeResult = largeResult - mean(largeResult);
+    
+    gain = 200;
+    smallResult = gain*eps*rand(1,100*1000);
+    smallResult = smallResult - mean(smallResult);
+    
+    decimalDigits = 12:17;
     % 
     % Conversion to N- digit precision
     for k = 1:numel(decimalDigits)
         nDigits = decimalDigits(k);
-        resultRepresentations(k,:) = round(result,nDigits);
-        if (any(abs(result-squeeze(resultRepresentations(k,:)))>=eps/2))
-            UnitTest.validationRecord('FAILED', sprintf('result is not represented accurately with %d decimal digits.', nDigits));
+        largeResultRepresentations(k,:) = round(largeResult,nDigits);
+        diff = abs(largeResult-squeeze(largeResultRepresentations(k,:)));
+        if (any(diff >= eps))
+            UnitTest.validationRecord('FAILED', sprintf('large result is not represented accurately with %d decimal digits.', nDigits));
         else
-            UnitTest.validationRecord('PASS', sprintf('result is represented accurately with %d decimal digits.', nDigits));
+            UnitTest.validationRecord('PASS', sprintf('large result is represented accurately with %d decimal digits.', nDigits));
         end
     end
-
+    
+    for k = 1:numel(decimalDigits)
+        nDigits = decimalDigits(k);
+        smallResultRepresentations(k,:) = round(smallResult,nDigits);
+        diff = abs(smallResult-squeeze(smallResultRepresentations(k,:)));
+        if (any(diff >= eps))
+            UnitTest.validationRecord('FAILED', sprintf('small result is not represented accurately with %d decimal digits.', nDigits));
+        else
+            UnitTest.validationRecord('PASS', sprintf('small result is represented accurately with %d decimal digits.', nDigits));
+        end
+    end
+    
     % append to validationData
-    UnitTest.validationData('resultDouble', result);
-    UnitTest.validationData('resultRepresentations', resultRepresentations);
+    UnitTest.validationData('largeResult', largeResult);
+    UnitTest.validationData('resultRepresentations', largeResultRepresentations);
+    UnitTest.validationData('smallResult', smallResult);
+    UnitTest.validationData('smallRepresentations', smallResultRepresentations);
     
     %% Plotting
     if (runTimeParams.generatePlots)
-        h = figure(1);
-        set(h, 'Position', [100 100  1430 780]);
-        clf;
-        for k = 1:numel(decimalDigits)
-            nDigits = decimalDigits(k);
-            subplot(3,6,k);
-            plot(result, abs(result-squeeze(resultRepresentations(k,:))), 'b.');
-            hold on;
-            plot(min(result)+[0 eps], 0.5*[eps eps], 'r-');
- 
-            hold off
-            set(gca, 'XLim', min(result)+[0 eps], 'YLim', [0 eps], ...
-                 'YTick', [0 eps/2 eps], 'YTickLabel', {'0', 'eps/2', 'eps'});
-            set(gca, 'FontSize', 12, 'FontName', 'Helvetica');
-            axis 'square'
-            if (k > 12)
-                xlabel('value', 'FontSize', 14, 'FontName', 'Helvetica', 'FontWeight', 'b');
-            end
-            if (mod(k-1,6) == 0)
-                ylabel('value- round(value, nDigits)', 'FontSize', 14, 'FontName', 'Helvetica', 'FontWeight', 'b');
-            end
-            title(sprintf('nDigits = %d', nDigits), 'FontSize', 14, 'FontName', 'Helvetica');
-        end
-        
+        plotResults(1, decimalDigits, largeResult, largeResultRepresentations, 'Large Values');
+        plotResults(2, decimalDigits, smallResult, smallResultRepresentations, 'Small Values');  
     end
     
 end
+
+
+%% Helper plotting function
+function plotResults(figNum, decimalDigits, result, resultRepresentations, figName)
+    h = figure(figNum);
+    set(h, 'Position', [100 100  1100 1060], 'Name', figName);
+    clf;
+    subplotWidth = 0.82/6;
+    margin = 0.02;
+    for k = 1:numel(decimalDigits)
+        nDigits = decimalDigits(k);
+        subplot('Position', [0.07+(k-1)*(subplotWidth+margin), 0.05, subplotWidth 0.90]);
+        diff = abs(result-squeeze(resultRepresentations(k,:)));
+        plot(result, diff, 'b.');
+        hold on; 
+        plot([min(result) max(result)], 0.5*[eps eps], 'r-');
+        
+        hold off
+        set(gca, 'XLim', [min(result) max(result)], 'YLim', [0 4*eps], ...
+             'YTick', [0 eps/2 eps eps*2 eps*4], 'YTickLabel', {'0', 'eps/2', 'eps', 'eps*2', 'eps*4'});
+        set(gca, 'FontSize', 12, 'FontName', 'Helvetica');
+
+        xlabel('value', 'FontSize', 14, 'FontName', 'Helvetica', 'FontWeight', 'b');
+        if (mod(k-1,6) == 0)
+            ylabel('| value - round(value,nDigits) |', 'FontSize', 14, 'FontName', 'Helvetica', 'FontWeight', 'b');
+        else
+           set(gca, 'YTick', []) 
+        end
+        title(sprintf('nDigits = %d', nDigits), 'FontSize', 14, 'FontName', 'Helvetica');
+    end
+end
+        
