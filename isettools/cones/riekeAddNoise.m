9ab909ad87183ca18c60abfa6322940dcaa93fa7
Haomiao Jiang <hjiang36@gmail.com>
2014-12-25 18:46:39 -0800

    Implement ARMA model simulation in riekeAddNoise
    Now we could simulate ARMA model in matrix form (note, this is not
    Vector ARMA model, just applying ARMA to matrix). This implementation
    will no longer depend on functions from economics toolbox and is much
    more efficient.

--- isettools/cones/riekeAddNoise.m
+++ isettools/cones/riekeAddNoise.m
@@ -19,13 +19,12 @@ function [adaptedCur, params] = riekeAddNoise(curNF, params)
 %  See also:
 %    coneAdapt, riekeAdaptSteadyState, riekeAdaptTemporal
 %
-%  (HJ) ISETBIO, 2014
-
-%% Programming Notes:
-%    For arima model simulation, matlab function is very slow. And also, it
-%    requires economics toolbox.
-%    HJ will write a function to handle this problem
+%  Note:
+%    Instead of using the arima model simulation functions in economics
+%    toolbox, we implement a simple version for the simulation. This is for
+%    the sake of speed and those without economics toolbox.
 %
+%  (HJ) ISETBIO, 2014
 
 %% Init
 if notDefined('curNF'), error('noise-free adapted current required'); end
@@ -40,33 +39,23 @@ end
 %  Make sure curNF is [row, col, time] 3D matrix
 if isvector(curNF), curNF = reshape(curNF, [1 1 length(curNF)]); end
 
-%  compute size
-[c, r, nFrames] = size(curNF);
-
 % build arima model
 if sampTime == 5e-5
     % This is the noise model fitted for data sampled at 50 micro-seconds
     % In this case, the noise is temporally correlated and we can fit it to
     % a ARIMA(3,0,2) model, see s_riekeNoise for further analysis
-    mdl = arima('AR', [1.518 -0.969923 0.421612], ...
-        'D', 0, ...
-        'MA', [1.11884 0.498774], ...
-        'Variance', 0.144268, ...
-        'Constant', 7.18207e-5);
-    noise = simulate(mdl, nFrames * c * r);
-    noise = permute(reshape(noise, [nFrames c r]), [2 3 1]);
+    ar = [1.518 -0.969923 0.421612];
+    ma = [1.11884 0.498774];
+    sigma = 0.3798;
+    noise = simulateARMA(ar, ma, sigma, size(curNF)); 
     adaptedCur = curNF + noise;
     
 elseif sampTime == 0.001
     % When data sampling interval is 1 ms, the noise is weakly correlated
     % and can be fitted to a ARIMA(1,0,1) model
-    mdl = arima('AR', 0.716881, ...
-        'D', 0, ...
-        'MA', -0.272412, ...
-        'Variance', 14.9429, ...
-        'Constant', 0.00108364);
-    noise = simulate(mdl, nFrames * c * r);
-    noise = permute(reshape(noise, [nFrames c r]), [2 3 1]);
+    ar = 0.716881; ma = -0.272412;
+    sigma = 3.8656;
+    noise = simulateARMA(ar, ma, sigma, size(curNF));
     adaptedCur = curNF + noise;
     
 elseif sampTime > 0.005
@@ -77,3 +66,64 @@ end
 
 end
 
+%% Aux functions
+function armaData = simulateARMA(ar, ma, sigma, sz)
+% Generate samples for ARMA model
+%  armaData = simulateARMA(ar, ma, sigma, sz)
+%
+%    This function generate samples for ARMA model
+%    It is similar to function simulate in economics toolbox
+%    We implement a simple version of that for speed and for those without
+%    that toolbox
+%
+%  Inputs:
+%    ar    - vector, containing auto-regressive coefficients
+%    ma    - vector, containing moving average coefficients
+%    simga - scalar, standard deviation of generation noise
+%    sz    - size, could be [rows, cols, nFrames]
+%
+%  Outputs:
+%
+%
+%  See also:
+%    arima, estimate, simulate
+%
+%  (HJ) ISETBIO TEAM, 2014
+
+%% Check inputs
+if ~exist('ar', 'var'), error('ar coefficients required'); end
+if ~exist('ma', 'var'), error('ma coefficients required'); end
+if ~exist('sigma', 'var'), error('std of noise required'); end
+if ~exist('sz', 'var'), error('simulation size (nFrames) required'); end
+
+if ~isscalar(sigma), error('std of noise should be a scalar'); end
+sz = padarray(sz(:), [3-numel(sz) 0], 1, 'pre')';
+
+%% Generate noise
+% will throw away first 20 more samples to avoid initial problems
+sz(3) = sz(3) + 20; 
+wt = randn(sz) * sigma;
+armaData = wt;
+
+for t = 2 : sz(3)
+    % auto-regressive part
+    indx = 1 : min(length(ar), t - 1);
+    indx = reshape(indx, [1 1 length(indx)]);
+    if ~isempty(indx)
+        arSum = sum(bsxfun(@times, ar(indx), armaData(:,:,t-indx(:))), 3);
+        armaData(:,:,t) = armaData(:,:,t) + arSum;
+    end
+    
+    % moving average part
+    indx = 1 : min(length(ma), t - 1);
+    indx = reshape(indx, [1 1 length(indx)]);
+    if ~isempty(indx)
+        maSum = sum(bsxfun(@times, ma(indx), wt(:,:,t-indx(:))), 3);
+        armaData(:,:,t) = armaData(:,:,t) + maSum;
+    end
+end
+
+% throw away the first 20 samples
+armaData = armaData(:,:, 21:end);
+
+end
