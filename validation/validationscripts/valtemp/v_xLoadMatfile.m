7508dbabdbb8ebb656381af519ecf365d6691473
David Brainard <brainard@psych.upenn.edu>
2015-01-02 21:08:56 -0500

    Dealing with big file mess.
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

---
+++ validation/validationscripts/valtemp/v_xLoadMatfile.m
@@ -0,0 +1,24 @@
+function varargout = v_xLoadMatfile(varargin)
+%
+% Loads a matfile to mimic data produced by v_tempHCCompression
+%
+% Copyright ImagEval Consultants, LLC, 2012
+
+    varargout = UnitTest.runValidationRun(@ValidationFunction, nargout, varargin);
+end
+
+
+%% Here is the actual code
+function ValidationFunction(runTimeParams)
+
+%% Init
+ieInit;
+
+%% Load
+curdir = pwd;
+cd(fileparts(mfilename('fullpath')));
+load
+cd(curdir);
+
+%% End
+end
