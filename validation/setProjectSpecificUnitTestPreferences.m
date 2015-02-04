35603fa286f2193725d4f55ad09169d54fa023a8
np_cottaris <cottaris@sas.upenn.edu>
2015-02-04 18:31:28 -0500

    Improvement in preferences organization.

--- validation/setProjectSpecificUnitTestPreferences.m
+++ validation/setProjectSpecificUnitTestPreferences.m
@@ -1,34 +1,22 @@
 % Method to set ISETBIO-specific preferences. Generally, this script should be run once only.
 function setProjectSpecificUnitTestPreferences
-    % set the project name
-    projectName = 'isetbioValidation';
-    
+
+    p = struct(...
+            'projectName',           'isetbioValidation', ...                                                                         % The project's name (also the preferences group name)
+            'validationRootDir',     fullfile(isetbioRootPath, 'validation'), ...                                                     % The directory that contains the 'scripts' directory.
+            'alternateFastDataDir',  '',  ...                                                                                         % Alternate FAST (hash) data directory location. Specify '' to use the default location, i.e., $validationRootDir/data/fast
+            'alternateFullDataDir',  fullfile(filesep,'Users1', 'Shared', 'Dropbox', 'ISETBIOFullValidationData'), ...                % Alternate FULL data directory location. Specify '' to use the default location, i.e., $validationRootDir/data/full
+            'clonedWikiLocation',    fullfile(filesep,'Users',  'Shared', 'Matlab', 'Toolboxes', 'ISETBIO_Wiki', 'isetbio.wiki'), ... % The local path to the directory where the wiki is cloned. This is only used for publishing tutorials.
+            'clonedGhPagesLocation', fullfile(filesep,'Users',  'Shared', 'Matlab', 'Toolboxes', 'ISETBIO_GhPages', 'isetbio'), ...   % The local path to the directory where the gh-pages repository is cloned. This is only used for publishing tutorials.
+            'githubRepoURL',         'http://isetbio.github.io/isetbio' ...                                                           % The github URL for the project. This is only used for publishing tutorials.
+        );
+
     % remove any existing preferences for this project
-    if ispref(projectName)
-        rmpref(projectName);
+    if ispref(p.projectName)
+        rmpref(p.projectName);
     end
     
     % generate and save the project-specific preferences
-    setpref(projectName, 'projectSpecificOptions', generateISETbioSpecificPreferences);
-    fprintf('Generated and saved preferences specific to the ''%s'' project.\n', projectName);
-end
-
-function p = generateISETbioSpecificPreferences
-    % The root directory that contains the 'scripts' directory.
-    p.validationRootDir = fullfile(isetbioRootPath, 'validation');
-    
-    % Alternate FAST (hash) data directory location. Specify '' to use the default location, i.e., $validationRootDir/data/fast
-    p.alternateFastDataDir = '';
-    
-    % Alternate FULL data directory location. Specify '' to use the default location, i.e., $validationRootDir/data/full
-    p.alternateFullDataDir = fullfile(filesep,'Users1', 'Shared', 'Dropbox', 'ISETBIOFullValidationData');
-    
-    % The local path to the directory where the wiki is cloned. This is only used for publishing tutorials.
-    p.clonedWikiLocation = fullfile(filesep,'Users', 'Shared', 'Matlab', 'Toolboxes', 'ISETBIO_Wiki', 'isetbio.wiki');
-    
-    % The local path to the directory where the pages is cloned. This is only used for publishing tutorials.
-    p.clonedGhPagesLocation = fullfile(filesep,'Users', 'Shared', 'Matlab', 'Toolboxes', 'ISETBIO_GhPages', 'isetbio');
-    
-    % The github URL for the project. This is only used for publishing tutorials.
-    p.githubRepoURL = 'http://isetbio.github.io/isetbio';
+    setpref(p.projectName, 'projectSpecificOptions', p);
+    fprintf('Generated and saved preferences specific to the ''%s'' project.\n', p.projectName);
 end
