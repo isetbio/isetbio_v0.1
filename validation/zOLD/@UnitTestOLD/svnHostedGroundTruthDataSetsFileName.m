544d898c719f309cc7a1b8450efae6b624bd9df0
np_cottaris <cottaris@sas.upenn.edu>
2015-02-04 18:33:11 -0500

    Removed zOLD

--- validation/zOLD/@UnitTestOLD/svnHostedGroundTruthDataSetsFileName.m
+++
@@ -1,8 +0,0 @@
- % Method to return the filename of the SVN-hosted ground truth data set
-function dataSetFilename = svnHostedGroundTruthDataSetsFileName(obj)
-    fullPathDataSetFileName = obj.groundTruthDataSetsFileName;    
-    
-    [pathstr,name,ext]  = fileparts(fullPathDataSetFileName);
-    obj.ISETBIO_DataSets_Local_SVN_DIR = sprintf('%s/SVNDIR',pathstr);
-    dataSetFilename = sprintf('%s/%s%s', obj.ISETBIO_DataSets_Local_SVN_DIR, name,ext);
-end
